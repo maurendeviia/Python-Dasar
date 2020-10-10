1dcc4a1ea3195cb52880633cdb9abb5c111f7f82
paramaDEV <admaja404@gmail.com>
2020-10-10 09:02:34 +0700

    Tambah array

---
+++ array.py
@@ -0,0 +1,11 @@
+# Membuat Array
+
+Mahasiswa = ["Anto","Komar","Seto","Mauren"]
+
+# Menampilkan Isi  Array
+print (Mahasiswa[0])
+
+#Menampilkan Seluruh Isi Array
+for i in Mahasiswa :
+    print(i)
+
